cfb12a31aea8558c31f0c40ebeed64e519e6fcdc
Tiago Gomes <tidgomes@gmail.com>
2023-08-15 11:27:07 -0300

    refactor NewTask component to use useHttp custom hook

--- real/src/components/NewTask/NewTask.js
+++ real/src/components/NewTask/NewTask.js
@@ -1,41 +1,29 @@
-import { useState } from 'react'
-
+import useHttp from '../../hooks/useHttp'
 import Section from '../UI/Section'
 import TaskForm from './TaskForm'
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const { sendRequest: sendTaskRequest, isLoading, error } = useHttp()
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText }
+
+    props.onAddTask(createdTask)
+  }
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true)
-    setError(null)
-    try {
-      const response = await fetch(
-        'https://temp-9a330-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
+    sendTaskRequest(
+      {
+        url: 'https://temp-9a330-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        body: { text: taskText },
+        headers: {
+          'Content-Type': 'application/json'
         }
-      )
-
-      if (!response.ok) {
-        throw new Error('Request failed!')
-      }
-
-      const data = await response.json()
-
-      const generatedId = data.name // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText }
-
-      props.onAddTask(createdTask)
-    } catch (err) {
-      setError(err.message || 'Something went wrong!')
-    }
-    setIsLoading(false)
+      },
+      createTask.bind(null, taskText)
+    )
   }
 
   return (
